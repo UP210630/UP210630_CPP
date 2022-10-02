14c35cc2c55536e70f0cb20e5faab5ec5ab66312
unknown <up210360@alumnos.upa.edu.mx>
2022-10-02 15:15:03 -0500

    Hide h

---
+++ .vscode/.history/UP210630_CPP/Programation/U1/01_HelloWorld_20221002145859.cpp
@@ -0,0 +1,26 @@
+/*
+Date: Sep/14th/2022
+Author: Miguel Alejandro Alba Sánchez
+Email: up210630@alumnos.upa.edu.mx
+Description: Doing my first program on C++ a Hello World (again).
+Last Modification: Sep/14th/2022
+*/
+
+//------------ Preprocessor Directives ----------------
+
+// library for input and output between screen and keyboard
+#include <iostream>
+
+// ----------- Main Function -------------
+
+int main()
+{
+	std::cout << "The Typical Hello World !! Written from C++ Hello World !!" ; // Write a message on the screen
+
+	/*
+	 If there are no errors, the program ends its execution.
+	*/
+
+    //Return 0 means that the program ands succesfully
+	return 0;
+}
